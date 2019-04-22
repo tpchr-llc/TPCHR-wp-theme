495dbf25e75db2dd008d6d49b80464c91f4e8eab
tpchr admin <admin@tpchr.onmicrosoft.com>
2019-04-22 09:58:47 -0500

    Comments are setup on the theme for the blop posting.

--- content.php
+++ content.php
@@ -6,6 +6,11 @@
                 </h2>
                 <p class="blog-post-meta">
                     <?php the_date(); ?> by <a href="#"><?php the_author(); ?></a>
+                    <a href="<?php comments_link(); ?>">
+                        <?php
+                            printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 		get_comments_number() ) );
+                        ?>
+                    </a>
                 </p>
                 <?php the_excerpt(); ?>
             </div><!-- /.blog-post -->

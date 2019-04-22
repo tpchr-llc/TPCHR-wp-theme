7a9428ab176e5899506578a4890f396f9bb843c7
tpchr admin <admin@tpchr.onmicrosoft.com>
2019-04-22 07:52:20 -0500

    Uploading all my start up files for the WordPress theme.

---
+++ footer.php
@@ -0,0 +1,9 @@
+        </div> <!-- /.container -->
+        <footer class="blog-footer">
+            <p>
+                <a href="#">Back to top</a>
+            </p>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>

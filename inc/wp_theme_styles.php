d54161d48a4e99e070ce721029e133da33d3053a
tpchr admin <admin@tpchr.onmicrosoft.com>
2019-05-07 14:48:58 -0500

    Re-organized my sass

--- inc/wp_theme_styles.php
+++ inc/wp_theme_styles.php
@@ -5,11 +5,7 @@ function TPCHR_wp_theme_styles() {
     wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array(), 1, 'all');
     wp_enqueue_style('bootstrap');
     // Theme Style
-    wp_register_style('global', get_template_directory_uri() . '/css/global.css', array(), 1, 'all');
-    wp_enqueue_style('global');
-    wp_register_style('header', get_template_directory_uri() . '/css/header.css', array(), 1, 'all');
-    wp_enqueue_style('header');
-    wp_register_style('footer', get_template_directory_uri() . '/css/footer.css', array(), 1, 'all');
-    wp_enqueue_style('footer');
+    wp_register_style('theme-style', get_template_directory_uri() . '/css/theme.css', array(), 1, 'all');
+    wp_enqueue_style('theme-style');
 }
 add_action('wp_enqueue_scripts', 'TPCHR_wp_theme_styles' );
